3c8fe91e7cc47ec46c8ebd1609ff3539f9a6c69b
hurou927 <god.be.with.ye.fs@gmail.com>
2022-03-24 22:20:37 +0900

    measure 1-1024MB

---
+++ README.md
@@ -0,0 +1,63 @@
+# SYCL: memcpy(d to d)
+
+## Build & Run
+
+```sh
+$ make
+$ ./main
+```
+
+## Result 
+
+- CPU: 11th Gen Intel(R) Core(TM) i7-11700K @ 3.60GHz
+- GPU: Intel(R) UHD Graphics 750
+- Memory: Crucial Technology CT16G4DFRA32A.M16FR
+  - Array Handle: 0x003B
+  - Error Information Handle: Not Provided
+  - Total Width: 64 bits
+  - Data Width: 64 bits
+  - Size: 16384 MB
+  - Form Factor: DIMM
+  - Set: None
+  - Locator: Controller0-ChannelA-DIMM1
+  - Bank Locator: BANK 0
+  - Type: DDR4
+  - Type Detail: Synchronous
+  - Speed: 3200 MT/s
+  - Manufacturer: Crucial Technology
+  - Serial Number: *******
+  - Asset Tag: *******
+  - Part Number: CT16G4DFRA32A.M16FR
+  - Rank: 2
+  - Configured Memory Speed: 3200 MT/s
+  - Minimum Voltage: 1.2 V
+  - Maximum Voltage: 1.2 V
+  - Configured Voltage: 1.2 V
+  - Memory Technology: DRAM
+  - Memory Operating Mode Capability: Volatile memory
+  - Firmware Version: Not Specified
+  - Module Manufacturer ID: Bank 6, Hex 0x9B
+  - Module Product ID: Unknown
+  - Memory Subsystem Controller Manufacturer ID: Unknown
+  - Memory Subsystem Controller Product ID: Unknown
+  - Non-Volatile Size: None
+  - Volatile Size: 16 GB
+  - Cache Size: None
+  - Logical Size: None
+
+```tsv
+Running on device: Intel(R) UHD Graphics 750 [0x4c8a]
+
+size  MB  memcpy(sub)  ms  memcpy(exe)  ms  memcpy(sub)  GB/s  kernel(sub)  ms  kerenel(exe)  ms  kerenel(sub)  GB/s
+1     MB  0.809795     ms  0.040006     ms  24.4104      GB/s  0.130729     ms  0.044322      ms  22.0334       GB/s
+2     MB  0.578274     ms  0.064408     ms  30.3243      GB/s  0.0913       ms  0.061752      ms  31.6285       GB/s
+4     MB  1.27648      ms  0.131472     ms  29.7116      GB/s  0.12729      ms  0.092296      ms  42.3231       GB/s
+8     MB  2.23178      ms  0.300958     ms  25.9588      GB/s  0.27348      ms  0.222772      ms  35.0695       GB/s
+16    MB  4.47386      ms  0.686244     ms  22.7689      GB/s  1.04915      ms  0.887104      ms  17.6135       GB/s
+32    MB  9.64813      ms  1.43424      ms  21.7885      GB/s  1.84347      ms  1.72408       ms  18.1257       GB/s
+64    MB  18.9782      ms  3.23086      ms  19.3447      GB/s  3.55251      ms  3.404         ms  18.3608       GB/s
+128   MB  36.5769      ms  6.68299      ms  18.7042      GB/s  6.95571      ms  6.76998       ms  18.4639       GB/s
+256   MB  72.9654      ms  13.3278      ms  18.7578      GB/s  13.6568      ms  13.5129       ms  18.5008       GB/s
+512   MB  145.823      ms  27.4664      ms  18.2041      GB/s  27.4479      ms  27.2429       ms  18.3534       GB/s
+```
+
---
+++ main.cpp
@@ -0,0 +1,108 @@
+#include <CL/sycl.hpp>
+#include <cstddef>
+#include <iomanip>
+#include <iostream>
+
+using namespace cl::sycl;
+using namespace std;
+
+using ITEM_T = int;
+constexpr int NUM_THREADS_PER_GROUP = 64;
+
+class Event {
+public:
+  event memcpy_event;
+  double memcpy_elapsed_ms;
+  event kernel_event;
+  double kernel_elapesed_ms;
+};
+
+double get_submission_ms(event e) {
+  auto end = e.get_profiling_info<sycl::info::event_profiling::command_end>();
+  auto start =
+      e.get_profiling_info<sycl::info::event_profiling::command_start>();
+  return (end - start) / 1.0e6;
+};
+
+double elaspsed_ms(std::chrono::steady_clock::time_point &start,
+                   std::chrono::steady_clock::time_point &end) {
+  return std::chrono::duration_cast<std::chrono::nanoseconds>(end - start)
+             .count() /
+         1e6;
+};
+
+template <typename T> Event run_kernel(queue &q, size_t num_items) {
+  assert(num_items % NUM_THREADS_PER_GROUP == 0);
+  T *DEVICE_READ = static_cast<T *>(malloc_device(num_items * sizeof(T), q));
+  T *DEVICE_WRITE = static_cast<T *>(malloc_device(num_items * sizeof(T), q));
+
+  auto memcpy_start = std::chrono::steady_clock::now();
+  event memcpy_event =
+      q.memcpy(DEVICE_WRITE, DEVICE_READ, sizeof(T) * num_items);
+  memcpy_event.wait();
+  auto memcpy_end = std::chrono::steady_clock::now();
+  auto memcpy_elapsed_ms = elaspsed_ms(memcpy_start, memcpy_end);
+
+  auto kernel_start = std::chrono::steady_clock::now();
+  event kernel_event = q.submit([&](handler &cgh) {
+    auto localRange = range<1>(NUM_THREADS_PER_GROUP);
+    auto kernel = [=](nd_item<1> it) {
+      auto localX = it.get_local_id(0);
+      auto globalX = it.get_global_id(0);
+      T x = globalX;
+      x = DEVICE_READ[globalX];
+      DEVICE_WRITE[globalX] = x;
+    };
+    cgh.parallel_for<class pm>(nd_range<1>{range<1>(num_items), localRange},
+                               kernel);
+  });
+  kernel_event.wait();
+  auto kernel_end = std::chrono::steady_clock::now();
+  auto kernel_elapsed_ms = elaspsed_ms(kernel_start, kernel_end);
+  free(DEVICE_READ, q);
+  free(DEVICE_WRITE, q);
+
+  return Event{
+      .memcpy_event = memcpy_event,
+      .memcpy_elapsed_ms = memcpy_elapsed_ms,
+      .kernel_event = kernel_event,
+      .kernel_elapesed_ms = kernel_elapsed_ms,
+  };
+}
+
+void copy(queue &q, size_t bytes) {
+  size_t mbytes = bytes / 1024 / 1024;
+  size_t num_items = bytes / sizeof(ITEM_T);
+  auto result = run_kernel<ITEM_T>(q, num_items);
+  cout << mbytes << ",MB,"
+       << result.memcpy_elapsed_ms << ",ms,"
+       << get_submission_ms(result.memcpy_event) << ",ms,"
+       << mbytes / get_submission_ms(result.memcpy_event) * 1e3 / 1024 << ",GB/s,"
+
+       << result.kernel_elapesed_ms << ",ms,"
+       << get_submission_ms(result.kernel_event) << ",ms,"
+       << mbytes / get_submission_ms(result.kernel_event) * 1e3 / 1024 << ",GB/s,"
+       << "\n";
+}
+
+int main(int argc, char *argv[]) {
+
+  gpu_selector d_selector;
+  queue q(d_selector);
+  cout << "device to device memory copy latency" << "\n";
+  cout << "Running on device: " << q.get_device().get_info<info::device::name>()
+       << "\n";
+  
+  cout << "size" << ",MB,"
+       << "memcpy(sub)" << ",ms,"
+       << "memcpy(exe)" << ",ms,"
+       << "memcpy(sub)" << ",GB/s,"
+       << "kernel(sub)" << ",ms,"
+       << "kerenel(exe)" << ",ms,"
+       << "kerenel(sub)" << ",GB/s,"
+       << "\n";
+  for(size_t mb = 1 ; mb <= 1024; mb = mb << 1 ){
+    copy(q, mb * 1024 * 1024);
+  }
+  return 0;
+}
